2f66f5a1085b574636190f898fcec3468a4dcbb7
Dirk Holtwick <dirk.holtwick@gmail.com>
2012-12-09 19:01:41 +0100

    Readded the credits to Ole Zorn, removed them before just to have shorter headers :)

--- Classes/HOPopoverViewController.m
+++ Classes/HOPopoverViewController.m
@@ -1,5 +1,7 @@
 //
-//  Copyright 2012 Dirk Holtwick, holtwick.it. All rights reserved.
+//  HOStringSense by Dirk Holtwick 2012, holtwick.it
+//  Based on OMColorSense by by Ole Zorn, 2012
+//  Licensed under BSD style license
 //
 
 #import "HOPopoverViewController.h"
