7834846986c231a04978a6550be541ed258b5b3c
Alex Gray <alex@mrgray.com>
2014-10-07 16:47:40 -0400

    Deleted header, moved implementation.

--- Classes/OMColorViews.m
+++ Classes/OMColorViews.m
@@ -1,7 +1,7 @@
 
 //  OMColorFrameView.m OMColorHelper   Created by Ole Zorn on 09/07/12.
 
-#import "OMColorFrameView.h"
+#import "OMColorViews.h"
 
 @implementation OMColorFrameView
 
@@ -15,5 +15,33 @@
 	if (color == _color) return; _color = color; [self setNeedsDisplay:YES];
 }
 
+@end
+
+@implementation OMPlainColorWell
+
+- (void)drawRect:(NSRect)_ {
+
+	[NSGraphicsContext saveGraphicsState];
+  NSRect fillRect = NSMakeRect(0, -5, self.bounds.size.width, self.bounds.size.height + 5);
+	NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:fillRect xRadius:5 yRadius:5];
+	[path addClip];
+	[self drawWellInside:self.bounds];
+	[NSGraphicsContext restoreGraphicsState];
+	
+	if (!self.strokeColor) return;
+
+  NSRect strokeRect = NSInsetRect(NSMakeRect(0, -5, self.bounds.size.width, self.bounds.size.height + 5),.5,.5);
+  NSBezierPath *strokePath = [NSBezierPath bezierPathWithRoundedRect:strokeRect xRadius:5 yRadius:5];
+  [self.strokeColor setStroke];
+  [strokePath stroke];
+}
+
+- (void) deactivate { [super deactivate]; [NSColorPanel.sharedColorPanel orderOut:nil]; }
+
+- (void)setStrokeColor:(NSColor *)strokeColor {
+
+	if (strokeColor == _strokeColor) return; _strokeColor = strokeColor; [self setNeedsDisplay:YES];
+}
 
 @end
+
--- Classes/OMPlainColorWell.m
+++ Classes/OMPlainColorWell.m
@@ -3,30 +3,3 @@
 
 #import "OMColorFrameView.h"
 
-@implementation OMPlainColorWell
-
-- (void)drawRect:(NSRect)_ {
-
-	[NSGraphicsContext saveGraphicsState];
-  NSRect fillRect = NSMakeRect(0, -5, self.bounds.size.width, self.bounds.size.height + 5);
-	NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:fillRect xRadius:5 yRadius:5];
-	[path addClip];
-	[self drawWellInside:self.bounds];
-	[NSGraphicsContext restoreGraphicsState];
-	
-	if (!self.strokeColor) return;
-
-  NSRect strokeRect = NSInsetRect(NSMakeRect(0, -5, self.bounds.size.width, self.bounds.size.height + 5),.5,.5);
-  NSBezierPath *strokePath = [NSBezierPath bezierPathWithRoundedRect:strokeRect xRadius:5 yRadius:5];
-  [self.strokeColor setStroke];
-  [strokePath stroke];
-}
-
-- (void) deactivate { [super deactivate]; [NSColorPanel.sharedColorPanel orderOut:nil]; }
-
-- (void)setStrokeColor:(NSColor *)strokeColor {
-
-	if (strokeColor == _strokeColor) return; _strokeColor = strokeColor; [self setNeedsDisplay:YES];
-}
-
-@end
