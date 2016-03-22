64fc27c2aa894240b95eada8757faef2dd68a6a3
Dirk Holtwick <dirk.holtwick@gmail.com>
2016-03-22 13:26:03 +0100

    Adding Xcode 7.3 support

--- Classes/HOStringFrameView.m
+++ Classes/HOStringFrameView.m
@@ -8,15 +8,13 @@
 
 @implementation HOStringFrameView
 
-- (void)drawRect:(NSRect)dirtyRect
-{
+- (void)drawRect:(NSRect)dirtyRect {
 	[self.color setStroke];
 	NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:NSInsetRect(self.bounds, 0.5, 0.5) xRadius:3.0 yRadius:3.0];
 	[path stroke];
 }
 
-- (void)setColor:(NSColor *)color
-{
+- (void)setColor:(NSColor *)color {
 	if (color != _color) {
 		_color = color;
 		[self setNeedsDisplay:YES];
